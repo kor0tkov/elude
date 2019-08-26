0b709ae148244cbccc8a713b231bf5eb7378ed7d
kor0tkov <48534370+kor0tkov@users.noreply.github.com>
2019-08-27 01:59:36 +0500

    Update mail.php

--- src/php/mail.php
+++ src/php/mail.php
@@ -1,26 +1,28 @@
 <?php
  if($_POST)
  {
- $to_Email = "quanoxy@gmail.com"; 
- $subject = 'Thank you'; 
+  $user = $_POST;
+  $to_Email = "quanoxy@gmail.com"; 
+  $subject = 'Thank you'; 
+  $message = 'hello, my name is '.$user.name;
+  $response;
 
- if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+  if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
 
- $otvet_serv = json_encode(
- array( 
- 'text' => 'Возникла ошибка при отправке данных'
- ));
+    $response = json_encode(
+    array( 
+      'text' => 'Возникла ошибка при отправке данных'
+    ));
 
- die($otvet_serv);
- } 
+    die($response);
+  }s
 
- if(!mail($to_Email, $subject, $message, "From: info@webriz.ru \r\n"))
- {
- $otvet_serv = json_encode(array('text' => 'Не могу отправить почту! Пожалуйста, проверьте ваши настройки PHP почты.'));
- die($otvet_serv);
- }else{
- $otvet_serv = json_encode(array('text' => 'Спасибо! '.$user_Name .', ваше сообщение отправлено.'));
- die($otvet_serv);
- }
+  if(!mail($to_Email, $subject, $message, "From: your@mom.fuck \r\n")) {
+    $response = json_encode(array('text' => 'Не могу отправить почту! Пожалуйста, проверьте ваши настройки PHP почты.'));
+    die($response);
+    } else {
+    $response = json_encode(array('text' => 'Спасибо! '.$user_Name .', ваше сообщение отправлено.'));
+    die($response);
+  }
  }
  ?>
