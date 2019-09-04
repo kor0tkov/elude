33c897de710754afcf207469b9e49513e80128d8
wilOliw <prgrf@ya.ru>
2019-09-05 00:09:09 +0300

    change email

--- src/js/mail.php
+++ src/js/mail.php
@@ -3,9 +3,9 @@
 if ($_POST) {
   $user = $_POST;
 
-  $to_Email = 'hello@voynov.co';
+  $to_Email = 'hello@elude.co';
   $subject = 'New Elude Request';
-  $headers ="From: Elude Today <hello@eludetoday.com>\nReply-to:hello@eludetoday.com\nContent-Type: text/plain;";
+  $headers ="From: Elude Today <hello@elude.co>\nReply-to:hello@eludetoday.com\nContent-Type: text/plain;";
 
   $message = sprintf('Name: %s %sE-mail: %s %sCity: %s',
     $user['name'],PHP_EOL, $user['mail'], PHP_EOL, $user['city']
